39e858cc79101ff9af187638e5063f033c77d880
Joyce Tsai <ninomii1126@gmail.com>
2023-09-24 12:18:11 -0700

    m2 assignment

---
+++ js/script.js
@@ -0,0 +1,107 @@
+    
+// 1
+let someMonth
+function theMonth(){}
+let currentMonth
+let summerMonth
+let myLibraryFunction
+
+// 2
+let num = 1
+let str = 'abc'
+let isCanceled = true
+let test = null 
+
+// 3
+
+let age = (35 * 3 ) + 2
+let height = 6 * 0.8  
+
+// 4 & 5
+let strFirstName = 'Chun'
+let strLastName
+strLastName = 'Tsai'
+let strAddress
+let strCity = 'San Diego', strState='CA'
+let intZipCode
+let intYourAge
+let strReferralSource
+let blnMayWeContactYou
+
+// 6
+let test1 = 3 + 'abc'
+console.log(test1)
+
+// 7
+let test2 = true + 'abc'
+console.log(test2)
+
+let test3 = 20 + true
+console.log(test3)
+
+// 8
+let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."'
+console.log(someString)
+
+// 9
+let test4 = null
+let test5
+
+// 10
+// Use the unary typeof operator on various literals to return the following types within the console window: string, number, Boolean, object, and undefined.
+console.log(typeof 'hello')
+console.log(typeof 10)
+console.log(typeof false)
+console.log(typeof {name:'Chun', age:20})
+console.log(typeof test5)
+
+// 11
+window.alert('Hello' + 'Chun An Tsai' + ',welcome to the JavaScript class!')
+
+// 12
+let name = 'Chun An Tsai'
+window.alert(`Hello ${name},welcome to the JavaScript class!`)
+
+// 13 
+let test6 = 'JavaScript'
+window.alert(`Hello ${name},welcome to the ${test6} class!`)
+
+// 14
+window.alert(`Hello ${name},\nwelcome to the ${test6} class!`)
+
+// 15
+let enteredName = window.prompt('Enter your name')
+window.alert(`Hello ${enteredName},\nwelcome to the ${test6} class!`)
+
+// 16
+let className = window.prompt('Enter the class you are taking')
+window.alert(`Hello ${enteredName},\nwelcome to the ${className} class!`)
+
+// 17
+let x =10 
+let y =20
+console.log(x+y)
+
+// 18
+let x1= 20
+x1+=20
+console.log(x1)
+
+// 19
+let x2 = 20
+x2*=5
+console.log(x2)
+
+// 20
+let x3 = 20%3
+x3/=1
+console.log(x3)
+
+// 21
+console.log('happy' === 'happy')
+
+// 22
+console.log(3 > 4)
+
+
+
